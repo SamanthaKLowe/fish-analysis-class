d010cd7111245b9e9543225212fc08105504c551
Samantha Lowe <samantha.kae.lowe@gmail.com>
2023-12-05 18:25:24 +0000

    changed size cut off for new column

--- fish-analysis.R
+++ fish-analysis.R
@@ -3,4 +3,4 @@ library(dplyr)
 fish_data <- read.csv("Gaeta_etal_CLC_data.csv")
 
 fish_data_cat <- fish_data |>
-  mutate(lenght_cat = ifelse(length > 200, "big", "small"))
+  mutate(lenght_cat = ifelse(length > 300, "big", "small"))
